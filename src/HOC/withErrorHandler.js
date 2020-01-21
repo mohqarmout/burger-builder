8aa06d2f0904d81845f9d1a162702d7204b14ed7
Mohammed <mhmmade@gmail.com>
2020-01-21 16:53:09 +0200

    Add the global error handler

---
+++ src/HOC/withErrorHandler.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+import Modal from 'components/UI/Modal/Modal';
+
+const withErrorHandler = (WarppedComponent, axios) =>
+  class ErrorHoc extends Component {
+    state = {
+      error: false,
+    };
+
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({
+          error: null,
+        });
+        return req;
+      });
+
+      axios.interceptors.response.use(null, error => {
+        this.setState({
+          error,
+        });
+        return Promise.reject(error);
+      });
+    }
+
+    errorHandler = () => {
+      this.setState({
+        error: false,
+      });
+    };
+
+    render() {
+      const { error } = this.state;
+      return (
+        <>
+          <Modal modalClosed={this.errorHandler} show={error}>
+            {error.message}
+          </Modal>
+          <WarppedComponent {...this.props} />
+        </>
+      );
+    }
+  };
+export default withErrorHandler;
