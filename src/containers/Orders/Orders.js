fa26ce22966516ab33b0226df0472c7df3a156ac
Mohammed <mhmmade@gmail.com>
2020-02-05 18:12:54 +0200

    perform Order component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-
+import withErrorHandler from 'HOC/withErrorHandler';
+import Spinner from 'components/UI/Spinner/Spinner';
+import axios from 'axiosInstances';
 import Order from 'components/Order/Order';
 
 class Orders extends Component {
-  state = {};
+  state = {
+    data: [],
+    lodaing: false,
+  };
+
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+
+    const { data, status } = await axios.get('orders.json');
+
+    try {
+      if (status === 200) {
+        const cache = [];
+        Object.keys(data).map(orderID => {
+          cache.push({ id: orderID, ...data[orderID] });
+        });
+
+        this.setState({ data: cache, loading: false });
+      }
+    } catch (error) {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
-    return (
-      <div>
-        <Order />
-        <Order />
-      </div>
-    );
+    const { lodaing, data } = this.state;
+    let orders = data.map(({ id, ingredinets, price }) => (
+      <Order key={id} price={price} ingredinets={ingredinets} />
+    ));
+
+    if (lodaing) {
+      orders = <Spinner />;
+    }
+
+    return <div>{orders}</div>;
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
