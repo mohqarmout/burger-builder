e428897b79737b1b804b54f403bff108babd76d9
Mohammed <mhmmade@gmail.com>
2020-02-08 23:17:57 +0200

    add the active props

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import classes from './Button.module.css';
 
-const button = ({ btnType, clicked, children, type = 'button' }) => (
+const button = ({
+  btnType,
+  clicked,
+  children,
+  type = 'button',
+  active = true,
+}) => (
   // eslint-disable-next-line react/button-has-type
   <button
     className={[classes.Button, classes[btnType]].join(' ')}
     onClick={clicked}
     type={type}
+    disabled={!active}
   >
     {children}
   </button>
