9653715f78ffbfa2b664798627751389c7505516
Mohammed <mhmmade@gmail.com>
2020-02-27 17:26:30 +0200

    fix bug

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,24 @@
-/* eslint-disable import/prefer-default-export */
+export const burgerAction = {
+  addIngredient: 'addIngredient',
+  removeIngredient: 'removeIngredient',
+};
+
 const makeActionCreator = (type, ...argNames) => {
   return (...argsValue) => {
-    const action = { type };
+    const action = { type, payload: {} };
     argNames.forEach((_arg, index) => {
-      action[argNames[index]] = argsValue[index];
+      action.payload[argNames[index]] = argsValue[index];
     });
     return action;
   };
 };
 
-export const burgerAction = {
-  addIngredient: 'addIngredient',
-  removeIngredient: 'removeIngredient',
-};
+export const addIngredientAction = makeActionCreator(
+  burgerAction.addIngredient,
+  'ingredient',
+);
+
+export const removeIngredientAction = makeActionCreator(
+  burgerAction.removeIngredient,
+  'ingredient',
+);
