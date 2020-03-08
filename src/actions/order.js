51e4bf22aafada78359bd81ad0a7890815288620
Mohammed <mhmmade@gmail.com>
2020-03-08 12:02:04 +0200

    Setup get orders thunk

--- src/actions/order.js
+++ src/actions/order.js
@@ -1,7 +1,9 @@
 import { makeSynActionCreator } from 'utils';
+import order from 'components/Order/Order';
 
 export const orderActionNames = {
   postOrder: 'POST_ORDER',
+  getPost: 'POST_ORDER',
 };
 
 const purchaseBurger = makeSynActionCreator(
@@ -9,6 +11,8 @@ const purchaseBurger = makeSynActionCreator(
   'id',
   'ordersData',
 );
+
+const fetchOrder = makeSynActionCreator(orderActionNames.getPost, order);
 
 export const postOrederThunk = ordersData => async (dispatch, _, { axios }) => {
   try {
@@ -18,5 +22,21 @@ export const postOrederThunk = ordersData => async (dispatch, _, { axios }) => {
     return dispatch(purchaseBurger(data.name, ordersData));
   } catch (err) {
     return err;
+  }
+};
+
+export const getOrederThunk = () => async (dispatch, _, { axios }) => {
+  const { data, status } = await axios.get('orders.json');
+  try {
+    if (status === 200) {
+      const cache = [];
+      Object.keys(data).map(orderID => {
+        const { orederDate, ...rest } = data[orderID];
+        cache.push({ id: orderID, ...rest });
+      });
+      return dispatch(fetchOrder(cache));
+    }
+  } catch (error) {
+    return error;
   }
 };
