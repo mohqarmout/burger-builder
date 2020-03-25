90a8ae6a725ecb8977bce4f353331a174b9d8a6b
Mohammed <mhmmade@gmail.com>
2020-03-25 02:21:06 +0200

    test NavigationItms Component

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -3,7 +3,20 @@ import { shallow } from 'enzyme';
 import NavigationItems from './NavigationItems';
 import NavigationItem from './NavigationItem/NavigationItem';
 
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<NavigationItems />);
+});
+
 it('Render tow <NavigatinItems/> if not authenticated', () => {
-  const wrapper = shallow(<NavigationItems />);
   expect(wrapper.find(NavigationItem)).toHaveLength(2);
+});
+
+it('Render three <NavigatinItems/> if authenticated including the logout nav', () => {
+  wrapper.setProps({ isAuthenticated: 'true' });
+  expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  expect(
+    wrapper.contains(<NavigationItem link="/logout">logout</NavigationItem>),
+  ).toEqual(true);
 });
