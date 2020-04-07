950ef1ce435374b8d69ae061902138721b39a0f1
Mohammed <mhmmade@gmail.com>
2020-04-07 05:09:36 +0300

    Add  setupWrapper helper

--- src/components/Burger/OrderSummary/OrderSummary.test.js
+++ src/components/Burger/OrderSummary/OrderSummary.test.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import setupWrapper from 'test/helpers/setupWrapper';
 import OrderSummary from './OrderSummary';
 
 const props = {
@@ -14,22 +13,17 @@ const props = {
   purchaseContinued: jest.fn().mockImplementation(() => {}),
 };
 
-const setup = () => {
-  const wrapper = shallow(<OrderSummary {...props} />);
-  return wrapper;
-};
-
 test('BurgerBuilder render with out error', () => {
-  const wrapper = setup();
+  const wrapper = setupWrapper(OrderSummary, props);
   expect(wrapper.text()).toContain(
     'A delicious burger with the following ingredients',
   );
 });
 test('should render four [li] ', () => {
-  const wrapper = setup();
+  const wrapper = setupWrapper(OrderSummary, props);
   expect(wrapper.find('li')).toHaveLength(4);
 });
 test('should display the passed price rounded to 2', () => {
-  const wrapper = setup();
+  const wrapper = setupWrapper(OrderSummary, props);
   expect(wrapper.find('strong').text()).toContain('10.55');
 });
