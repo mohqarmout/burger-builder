5f7bf24ef7515d4c0a92186017b7d2231a0bd3e7
Mohammed <mhmmade@gmail.com>
2020-04-08 01:45:46 +0300

    Add BuildControls unit test

---
+++ src/components/Burger/BuildControls/BuildControls.test.js
@@ -0,0 +1,30 @@
+import setupWrapper from 'test/helpers/setupWrapper';
+import BuildControls from './BuildControls';
+import BuildControl from './BuildControl/BuildControl';
+
+const props = {
+  authenticated: true,
+  price: 10.5555,
+  disabled: {
+    salad: true,
+    bacon: true,
+    cheese: true,
+    meat: true,
+  },
+};
+test('BuildControls should render with out error', () => {
+  const wrapper = setupWrapper(BuildControls, props);
+  expect(wrapper).toHaveLength(1);
+});
+test(`should viwe 'ORDER NOW' when authenticated set to true`, () => {
+  const wrapper = setupWrapper(BuildControls, props);
+  expect(wrapper.text()).toContain('ORDER NOW');
+});
+test(`should round the value of price to tow number`, () => {
+  const wrapper = setupWrapper(BuildControls, props);
+  expect(wrapper.text()).toContain('10.56');
+});
+test(`should render four BuildControl`, () => {
+  const wrapper = setupWrapper(BuildControls, props);
+  expect(wrapper.find(BuildControl)).toHaveLength(4);
+});
