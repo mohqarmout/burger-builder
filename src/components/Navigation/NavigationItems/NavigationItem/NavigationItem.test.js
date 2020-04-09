bb89963d32e7f4f7219bfe219804e9ced901faaf
Mohammed <mhmmade@gmail.com>
2020-04-09 06:13:20 +0300

    replace snapshot to unit testing to NavigationItem component

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.test.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.test.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import setupShallowWrapper from 'test/helpers/setupShallowWrapper';
 import { NavLink } from 'react-router-dom';
 import NavigationItem from './NavigationItem';
 
 it('NavigationItem should render correctly', () => {
-  const component = shallow(<NavigationItem link="/" />);
-  expect(component).toMatchSnapshot();
+  const component = setupShallowWrapper(NavigationItem, { link: '/' });
+  expect(component).toHaveLength(1);
 });
-
 it('button click should hide component', () => {
   const clickFn = jest.fn();
-  const component = shallow(<NavigationItem link="/" closed={clickFn} />);
+  const component = setupShallowWrapper(NavigationItem, {
+    link: '/',
+    closed: clickFn,
+  });
   component.find(NavLink).simulate('click');
   expect(clickFn).toHaveBeenCalled();
 });
