c3a9d794140fa915e909e86ff8763d8ddbb756d0
Mohammed <mhmmade@gmail.com>
2020-05-09 13:58:57 +0300

    Update layout using hooks

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,44 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-  state = {
-    showSideDrawer: false,
+const Layout = props => {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+  const { children, isAuthenticated } = props;
+
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
   };
 
-  sideDrawerClosedHandler = () => {
-    this.setState({ showSideDrawer: false });
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
   };
 
-  sideDrawerToggleHandler = () => {
-    this.setState(({ showSideDrawer }) => {
-      return { showSideDrawer: !showSideDrawer };
-    });
-  };
-
-  render() {
-    const { showSideDrawer } = this.state;
-    const { children, isAuthenticated } = this.props;
-
-    return (
-      <>
-        <Toolbar
-          isAuthenticated={isAuthenticated}
-          drawerToggleClicked={this.sideDrawerToggleHandler}
-        />
-        <SideDrawer
-          isAuthenticated={isAuthenticated}
-          open={showSideDrawer}
-          closed={this.sideDrawerClosedHandler}
-        />
-        <main className={classes.Content}>{children}</main>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Toolbar
+        isAuthenticated={isAuthenticated}
+        drawerToggleClicked={sideDrawerToggleHandler}
+      />
+      <SideDrawer
+        isAuthenticated={isAuthenticated}
+        open={showSideDrawer}
+        closed={sideDrawerClosedHandler}
+      />
+      <main className={classes.Content}>{children}</main>
+    </>
+  );
+};
 
 const mapStateToProps = ({ auth: { token } }) => {
   return {
