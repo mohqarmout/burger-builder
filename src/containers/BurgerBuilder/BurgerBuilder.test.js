ee3a8f91d391f5668a4afdea269eb14f7840f928
Mohammed <mhmmade@gmail.com>
2020-05-11 16:39:36 +0300

    Add inject router utility

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { MemoryRouter, Route } from 'react-router-dom';
-import setupShallowWrapper from 'test/helpers/setupShallowWrapper';
+import injectRouter from 'test/utils/injectRouter';
 import Modal from 'components/UI/Modal/Modal';
 import Spinner from 'components/UI/Spinner/Spinner';
 import { BurgerBuilder } from './BurgerBuilder';
@@ -9,29 +6,15 @@ import { BurgerBuilder } from './BurgerBuilder';
 afterEach(() => {
   jest.restoreAllMocks();
 });
-
-const setup = props => {
-  return mount(
-    <MemoryRouter initialEntries={['/']} initialIndex={0}>
-      <Route
-        path="/"
-        render={routeProps => <BurgerBuilder {...props} {...routeProps} />}
-      />
-    </MemoryRouter>,
-  );
-};
-
 test('BurgerBuilder render with out error', () => {
-  const wrapper = setup();
+  const wrapper = injectRouter(BurgerBuilder, {}, '/');
   expect(wrapper.find(Modal).exists()).toBe(true);
-  console.log(wrapper.debug());
 });
 test('display error message when internal component error', () => {
-  const wrapper = setup();
-  wrapper.setState({ error: true });
+  const wrapper = injectRouter(BurgerBuilder, {}, '/');
   expect(wrapper.text()).toContain('sorry something went wrong');
 });
 test('render spinner when no ingredients', () => {
-  const wrapper = setup();
+  const wrapper = injectRouter(BurgerBuilder, {}, '/');
   expect(wrapper.find(Spinner).exists()).toBe(true);
 });
